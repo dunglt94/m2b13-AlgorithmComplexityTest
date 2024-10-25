921879a828988ebb183644b2a561902005d94aa3
dunglt94 <tuandung9594@gmail.com>
2024-10-25 16:39:05 +0700

    them phan danh gia su dung big-O

--- src/AlgorithmComplexityTest.java
+++ src/AlgorithmComplexityTest.java
@@ -6,19 +6,19 @@ public class AlgorithmComplexityTest {
         System.out.println("Enter a string:");
         String str = scanner.nextLine();
 
-        int[] frequentChars = new int[255];
-        for (int i = 0; i < str.length(); i++) {
-            int ascii = str.charAt(i);
-            frequentChars[ascii]++;
+        int[] frequentChars = new int[255];         // {1} : O(1)
+        for (int i = 0; i < str.length(); i++) {    // {2} : O((1 + 1).n) = O(1.n) = O(n)
+            int ascii = str.charAt(i);              // {3} : O(1)
+            frequentChars[ascii]++;                 // {4} : O(1)
         }
-        int max = 0;
-        char character = (char) 255;
-        for (int j = 0; j < 255; j++) {
-            if (frequentChars[j] > max) {
-                max = frequentChars[j];
-                character = (char) j;
+        int max = 0;                                //{5} : O(1)
+        char character = (char) 255;                //{6} : O(1)
+        for (int j = 0; j < 255; j++) {             //{7} : O(C(1 + 1).255) = O (1.255) = O(255)
+            if (frequentChars[j] > max) {           //{8}  : O(C)
+                max = frequentChars[j];             //{9} : O(1)
+                character = (char) j;               //{10}  : O(1)
             }
-        }
+        }                                           //T(n) = O(n) + O(255) = O(n)
         System.out.println("The most appearing letter is '" + character + "' with a frequency of " + max + " times");
     }
 }
